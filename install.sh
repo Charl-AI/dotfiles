3533945871a7fbddebab8fa1cca024e3da14674a
charlie <charlesjones9991@gmail.com>
2021-12-04 22:11:44 +0000

    working install.sh

--- install.sh
+++ install.sh
@@ -9,28 +9,28 @@ which git || sudo apt install -y git
 which curl || sudo apt install -y curl
 which wget || sudo apt install -y wget
 
-# Install fish
-which fish || sudo apt install -y fish
-
-# Make fish the default shell
-if [ $SHELL != $(which fish) ]; then chsh -s $(which fish); fi
-
-# install oh-my-fish
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-omf update omf
-
-# change default theme
-omf install robbyrussell
-omf theme robbyrussell
-
-# install z command
-omf install z
-
-# install fish config
-ln -sf $HOME/dotfiles/dotfiles/config.fish $HOME/.config/fish/config.fish
-
-# Install tmux
+echo "Install tmux"
 which tmux || sudo apt install -y tmux
 
-# Install my tmux conf
+echo "Creating SymLink to my tmux conf"
 ln -sf $HOME/dotfiles/dotfiles/.tmux.conf $HOME/.tmux.conf
+
+echo "Installing fish"
+which fish || sudo apt install -y fish
+
+echo "Installing Oh-My-Fish"
+ls $HOME/.local/share/omf/ || fish -c "curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish" << EOF
+EOF
+
+echo "Changing default theme"
+ls $HOME/.local/share/omf/themes/robbyrussell/ || fish -c "omf install robbyrussell"
+
+echo "Installing z command"
+ls $HOME/.local/share/omf/pkg/z/ || fish -c "omf install z"
+
+echo "Creating SymLink to my fish config"
+touch $HOME/.config/fish/config.fish
+ln -sf $HOME/dotfiles/dotfiles/config.fish $HOME/.config/fish/config.fish
+
+echo "Setting fish as default shell"
+if [ $SHELL != $(which fish) ]; then chsh -s $(which fish); fi
