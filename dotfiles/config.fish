41e976363ac837a7d7405249ff4d6d038fbc9f01
charlie <charlesjones9991@gmail.com>
2021-12-04 19:04:29 +0000

    create config.fish

---
+++ dotfiles/config.fish
@@ -0,0 +1,6 @@
+function fish_greeting
+    echo Hello $USER, have a nice day!
+    echo The time is (set_color yellow; date +%T; set_color normal) and this machine is called $hostname
+end
+
+set -x PIP_REQUIRE_VIRTUALENV true
