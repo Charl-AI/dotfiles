4cf8b5962c6777ea06e999c559594514de60b61f
charlie <charlesjones9991@gmail.com>
2021-12-27 13:51:23 +0000

    setup fish and tmux

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,7 +1,8 @@
 function fish_greeting
-    echo Hello $USER, have a nice day!
-    echo The time is (set_color yellow; date +%T; set_color normal) and this machine is called $hostname
+    echo Hi(set_color yellow) $USER(set_color normal)@(set_color yellow)$hostname(set_color normal),\
+    have a nice day! The time is (set_color yellow; date +%T;set_color normal)
+    echo TMUX status is:
+    tmux ls
 end
 
 set -x PIP_REQUIRE_VIRTUALENV true
-starship init fish | source
