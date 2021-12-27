642a29266bf9c80182ad3e3d6a71b4f08d713771
charlie <charlesjones9991@gmail.com>
2021-12-27 13:26:05 +0000

    use robbyrussell theme

--- install.sh
+++ install.sh
@@ -17,22 +17,14 @@ echo "Installing tmux"
 which tmux || sudo apt install -y tmux
 ln -sf $HOME/dotfiles/dotfiles/.tmux.conf $HOME/.tmux.conf
 
-
 echo "Installing fish"
 which fish || sudo apt install -y fish
 mkdir -p $HOME/.config/fish
 ln -sf $HOME/dotfiles/dotfiles/config.fish $HOME/.config/fish/config.fish
 
-echo "Installing starship prompt"
-which starship || sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
-ln -sf $HOME/dotfiles/dotfiles/starship.toml $HOME/.config/starship.toml
-
 echo "Installing Oh-My-Fish"
 ls $HOME/.local/share/omf/ || fish -c "curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish"
-
-# NOTE: Now use starship instead of robbyrussell, so commented out
-# echo "Changing default theme"
-# ls $HOME/.local/share/omf/themes/robbyrussell/ || fish -c "omf install robbyrussell"
+ls $HOME/.local/share/omf/themes/robbyrussell/ || fish -c "omf install robbyrussell"
 
 echo "Installing z command"
 ls $HOME/.local/share/omf/pkg/z/ || fish -c "omf install z"
