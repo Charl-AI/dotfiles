8f5991515ee2330274891eb91ce27870d388179b
Charles Jones <cj1917@ic.ac.uk>
2022-09-22 13:03:30 +0100

    just clean up script a bit

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -13,9 +13,11 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use { 'ggandor/leap.nvim' , config = function()
-          require('leap').set_default_keymaps()
-  end}
+  use { 'ggandor/leap.nvim' ,
+  	config = function()
+       	    require('leap').set_default_keymaps()
+    end
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
