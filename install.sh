2b787177a52d43c4dbfbcefb4070880a80831a9c
Charles Jones <cj1917@ic.ac.uk>
2022-10-29 16:40:51 +0100

    tweak install script

--- install.sh
+++ install.sh
@@ -3,33 +3,43 @@
 # Exit on first failure
 set -e
 
-# Prerequisites
+echo "Installing prerequisites"
 which sudo || apt-get install -y sudo
+sudo apt-get update -y
 yes | sudo apt-get install software-properties-common
 yes | sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository ppa:apt-fast/stable
 sudo apt-get update -y
-which git || sudo apt install -y git
-which curl || sudo apt install -y curl
-which wget || sudo apt install -y wget
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y apt-fast
 
-echo "Installing nano"
-which nano || sudo apt install -y nano
+echo "Install everything with apt-fast"
+DEBIAN_FRONTEND=noninteractive sudo apt-fast install -y \
+	git \
+	wget \
+	curl \
+	nano \
+	neovim \
+	tmux \
+	fish
+
+echo "SymLink dotfiles"
+
+# inputrc
+ln -sf $HOME/dotfiles/dotfiles/.inputrc $HOME/.inputrc
+
+# nano
 ln -sf $HOME/dotfiles/dotfiles/.nanorc $HOME/.nanorc
 
-echo "Installing neovim"
-sudo apt install -y neovim
+# neovim, need to run bootstrap script 2-3 times for some reason
 mkdir -p $HOME/.config/nvim
 ln -sf $HOME/dotfiles/dotfiles/init.lua $HOME/.config/nvim/init.lua
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
-echo "Installing tmux"
-which tmux || sudo apt install -y tmux
 ln -sf $HOME/dotfiles/dotfiles/.tmux.conf $HOME/.tmux.conf
 
-echo "Installing fish"
-which fish || sudo apt install -y fish
+# fish
 mkdir -p $HOME/.config/fish
 ln -sf $HOME/dotfiles/dotfiles/config.fish $HOME/.config/fish/config.fish
 
-# need to run this again to set up nvim properly for some reason
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
