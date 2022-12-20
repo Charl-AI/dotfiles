3acabfd282245d42bd778fff77a67ca81b6e9270
Charles Jones <cj1917@ic.ac.uk>
2022-12-20 14:53:29 +0000

    improve nvim config

--- install.sh
+++ install.sh
@@ -8,14 +8,20 @@ which sudo || apt-get install -y sudo
 sudo apt-get update -y
 yes | sudo apt-get install software-properties-common
 yes | sudo add-apt-repository ppa:neovim-ppa/stable
-sudo add-apt-repository ppa:apt-fast/stable
 sudo apt-get update -y
 
 echo "Install everything"
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
-	git \
+	autoconf \
+        automake \
+        cmake \
+        coreutils \
+        curl \
+        git \
+        libtool \
+        pkgconf \
+        unzip \
 	wget \
-	curl \
 	nano \
 	neovim \
 	tmux \
@@ -23,19 +29,14 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
 
 echo "SymLink dotfiles"
 
-# inputrc
-ln -sf $HOME/dotfiles/dotfiles/.inputrc $HOME/.inputrc
-
 # nano
 ln -sf $HOME/dotfiles/dotfiles/.nanorc $HOME/.nanorc
 
-# neovim, need to run bootstrap script 2-3 times for some reason
+# neovim
 mkdir -p $HOME/.config/nvim
 ln -sf $HOME/dotfiles/dotfiles/init.lua $HOME/.config/nvim/init.lua
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
+# tmux
 ln -sf $HOME/dotfiles/dotfiles/.tmux.conf $HOME/.tmux.conf
 
 # fish
