731c4c9cad33ff78f23a720481500bec65104d63
C Jones <cj1917@ic.ac.uk>
2023-03-14 14:02:30 +0000

    add gh cli

--- install.sh
+++ install.sh
@@ -28,6 +28,15 @@ function install_packages {
 		ripgrep \
 		fish
 
+	# gh cli
+	type -p curl >/dev/null || sudo apt install curl -y
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+	# nvim
 	wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
 	sudo apt install ./nvim-linux64.deb
 	rm nvim-linux64.deb
