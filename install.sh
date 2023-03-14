59c7626dd736aa29b7e1f4cc7b7f84879b161ff9
C Jones <cj1917@ic.ac.uk>
2023-03-14 14:19:07 +0000

    add lazygit

--- install.sh
+++ install.sh
@@ -40,6 +40,14 @@ function install_packages {
 	wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
 	sudo apt install ./nvim-linux64.deb
 	rm nvim-linux64.deb
+
+	# lazygit
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf lazygit.tar.gz lazygit
+	sudo install lazygit /usr/local/bin
+	rm -rf lazygit
+	rm lazygit.tar.gz
 }
 
 # this should not require root privileges
