8b0f6ae593704ed05a55691e56661a48e337156f
C Jones <cj1917@ic.ac.uk>
2023-03-15 12:02:02 +0000

    update welcome message

--- dotfiles/nvim/lua/plugins/ui.lua
+++ dotfiles/nvim/lua/plugins/ui.lua
@@ -82,36 +82,15 @@ return {
         event = "VimEnter",
         opts = function()
         local dashboard = require("alpha.themes.dashboard")
-        local logo = [[
-            _____    ___   _   _       _
-            /  __ \  |_  | | \ | |     (_)
-            | /  \/    | | |  \| |_   ___ _ __ ___
-            | |        | | | . ` \ \ / / | '_ ` _ \
-            | \__/\/\__/ / | |\  |\ V /| | | | | | |
-            \____/\____/  \_| \_/ \_/ |_|_| |_| |_|
-
-
-            _   _                                                _         _             _
-            | | | |                                              | |       | |           | |
-            | |_| | __ ___   _____    __ _    __ _ _ __ ___  __ _| |_    __| | __ _ _   _| |
-            |  _  |/ _` \ \ / / _ \  / _` |  / _` | '__/ _ \/ _` | __|  / _` |/ _` | | | | |
-            | | | | (_| |\ V /  __/ | (_| | | (_| | | |  __/ (_| | |_  | (_| | (_| | |_| |_|
-            \_| |_/\__,_| \_/ \___|  \__,_|  \__, |_|  \___|\__,_|\__|  \__,_|\__,_|\__, (_)
-                                            __/ |                                  __/ |
-                                            |___/                                  |___/
-
-        ]]
-
-        dashboard.section.header.val = vim.split(logo, "\n")
         dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-        dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-        dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+        dashboard.button("f", " " .. " [f]ind file", ":Telescope find_files <CR>"),
+        dashboard.button("n", " " .. " [n]ew file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("r", " " .. " [r]ecent files", ":Telescope oldfiles <CR>"),
+        dashboard.button("g", " " .. " [g]rep text", ":Telescope live_grep <CR>"),
+        dashboard.button("c", " " .. " [c]onfig", ":e $MYVIMRC <CR>"),
+        dashboard.button("s", " " .. " [s]ession restore", [[:lua require("persistence").load() <cr>]]),
+        dashboard.button("l", "󰒲 " .. " [l]azy menu", ":Lazy<CR>"),
+        dashboard.button("q", " " .. " [q]uit", ":qa<CR>"),
         }
         for _, button in ipairs(dashboard.section.buttons.val) do
             button.opts.hl = "AlphaButtons"
