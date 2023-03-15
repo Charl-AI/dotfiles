71561e55973adcdaf5457cdabe1d3cb6a9b0de5a
C Jones <cj1917@ic.ac.uk>
2023-03-15 16:55:09 +0000

    add format on save

--- dotfiles/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/lua/config/autocmds.lua
@@ -10,21 +10,30 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- resize splits if window got resized
 local resize_splits_group = vim.api.nvim_create_augroup('ResizeSplits', { clear = true })
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-    group = resize_splits_group,
-    callback = function()
-      vim.cmd("tabdo wincmd =")
-    end,
-  })
+  group = resize_splits_group,
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
 
 -- go to last loc when opening a buffer
 local last_loc_group = vim.api.nvim_create_augroup('LastLoc', { clear = true })
 vim.api.nvim_create_autocmd("BufReadPost", {
-group = last_loc_group,
-callback = function()
+  group = last_loc_group,
+  callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
     if mark[1] > 0 and mark[1] <= lcount then
-    pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
     end
-end,
+  end,
+})
+
+-- format on save
+local fmt_on_save_group = vim.api.nvim_create_augroup('FmtOnSave', { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = fmt_on_save_group,
+  callback = function()
+    vim.lsp.buf.format()
+  end
 })
