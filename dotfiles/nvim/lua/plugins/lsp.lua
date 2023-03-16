82fd5827a52b7d55adaacd72c17b2ccae4e14913
C Jones <cj1917@ic.ac.uk>
2023-03-16 17:03:48 +0000

    copilot mostly setup

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -55,15 +55,30 @@ return {
 
   {
     "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
+    cmd   = "Copilot",
+    event = "InsertEnter",
     build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
+    opts  = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = false,
+          accept_word = "<C-l>",
+          accept_line = "<C-j>",
+          dismiss = "<C-e>",
+          next = "<C-n>",
+          previous = "<C-p>",
+        }
+      },
+      panel = {
+        enabled = true,
+        keymap = {
+          open = "<C-p>",
+        }
+      },
     },
   },
-
-
 
   {
     'hrsh7th/nvim-cmp',
@@ -94,10 +109,20 @@ return {
         },
         mapping = cmp.mapping.preset.insert(
           {
+            -- remove c-p default mapping
+            ["<C-p>"] = cmp.mapping(function(fallback)
+              fallback()
+            end, { "i", "s" }),
+            -- remove c-n default mapping
+            ["<C-n>"] = cmp.mapping(function(fallback)
+              fallback()
+            end, { "i", "s" }),
             ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
             ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
             ["<Tab>"] = cmp.mapping(function(fallback)
-              if cmp.visible() then
+              if require("copilot.suggestion").is_visible() then
+                require("copilot.suggestion").accept()
+              elseif cmp.visible() then
                 cmp.confirm()
               elseif has_words_before() then
                 cmp.complete()
