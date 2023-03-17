0f6e9b6feae4626bd6c7d4e7ce50bdabf53849d6
C Jones <cj1917@ic.ac.uk>
2023-03-17 12:17:37 +0000

    improve install file

--- install.sh
+++ install.sh
@@ -37,17 +37,15 @@ function install_packages {
 	&& sudo apt install gh -y
 
 	# nvim
-	wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-	sudo apt install ./nvim-linux64.deb
-	rm nvim-linux64.deb
+	wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb -P /tmp/
+	sudo apt install /tmp/nvim-linux64.deb
 
 	# lazygit
-	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-	tar xf lazygit.tar.gz lazygit
+	cd /tmp/ && \
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*') && \
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" && \
+	tar xf lazygit.tar.gz lazygit && \
 	sudo install lazygit /usr/local/bin
-	rm -rf lazygit
-	rm lazygit.tar.gz
 
 	# node (for copilot)
 	sudo dpkg --remove --force-remove-reinstreq libnode-dev
