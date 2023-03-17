874cb4f64560a82a4de7db064ca6f831db7644f8
Charles Jones <cj1917@ic.ac.uk>
2023-03-17 12:59:59 +0000

    format script

--- install.sh
+++ install.sh
@@ -30,27 +30,27 @@ function install_packages {
 
 	# gh cli
 	type -p curl >/dev/null || sudo apt install curl -y
-	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-	&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+		sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+		sudo apt update &&
+		sudo apt install gh -y
 
 	# nvim
 	wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb -P /tmp/
 	sudo apt install /tmp/nvim-linux64.deb
 
 	# lazygit
-	cd /tmp/ && \
-	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*') && \
-	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" && \
-	tar xf lazygit.tar.gz lazygit && \
-	sudo install lazygit /usr/local/bin
+	cd /tmp/ &&
+		LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*') &&
+		curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" &&
+		tar xf lazygit.tar.gz lazygit &&
+		sudo install lazygit /usr/local/bin
 
 	# node (for copilot)
 	sudo dpkg --remove --force-remove-reinstreq libnode-dev
 	sudo dpkg --remove --force-remove-reinstreq libnode72:amd64
-	curl -sL https://deb.nodesource.com/setup_16.x | sudo bash - && sudo apt-get install  -y nodejs
+	curl -sL https://deb.nodesource.com/setup_16.x | sudo bash - && sudo apt-get install -y nodejs
 }
 
 # this should not require root privileges
@@ -72,7 +72,6 @@ function link_dotfiles {
 	ln -sf $HOME/dotfiles/dotfiles/.gitconfig $HOME/.gitconfig
 
 }
-
 
 function main {
 	# if no arguments are passed, install everything and link dotfiles
