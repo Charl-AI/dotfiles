34c4a9e6b7491b1d6391f1a5d1ad0cddd7f86ace
C Jones <cj1917@ic.ac.uk>
2023-03-20 16:32:08 +0000

    improve lsp

--- dotfiles/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/lua/config/autocmds.lua
@@ -1,39 +1,30 @@
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 
 -- resize splits if window got resized
-local resize_splits_group = vim.api.nvim_create_augroup('ResizeSplits', { clear = true })
+local resize_splits_group = vim.api.nvim_create_augroup("ResizeSplits", { clear = true })
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-  group = resize_splits_group,
-  callback = function()
-    vim.cmd("tabdo wincmd =")
-  end,
+	group = resize_splits_group,
+	callback = function()
+		vim.cmd("tabdo wincmd =")
+	end,
 })
 
 -- go to last loc when opening a buffer
-local last_loc_group = vim.api.nvim_create_augroup('LastLoc', { clear = true })
+local last_loc_group = vim.api.nvim_create_augroup("LastLoc", { clear = true })
 vim.api.nvim_create_autocmd("BufReadPost", {
-  group = last_loc_group,
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
--- format on save
-local fmt_on_save_group = vim.api.nvim_create_augroup('FmtOnSave', { clear = true })
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = fmt_on_save_group,
-  callback = function()
-    vim.lsp.buf.format()
-  end
+	group = last_loc_group,
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
 })
