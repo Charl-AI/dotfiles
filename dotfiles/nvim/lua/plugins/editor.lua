999819e2b9b0790c6c84bac862b5a75e07ca14d7
C Jones <cj1917@ic.ac.uk>
2023-03-21 18:05:37 +0000

    remove unused type

--- dotfiles/nvim/lua/plugins/editor.lua
+++ dotfiles/nvim/lua/plugins/editor.lua
@@ -127,7 +127,6 @@ return {
 	{
 		"ggandor/flit.nvim",
 		keys = function()
-			---@type LazyKeys[]
 			local ret = {}
 			for _, key in ipairs({ "f", "F", "t", "T" }) do
 				ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
