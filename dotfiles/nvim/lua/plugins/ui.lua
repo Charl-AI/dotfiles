eb1fc9f0b8f1defddd37e801ccfd589599baa30f
C Jones <cj1917@ic.ac.uk>
2023-04-07 13:19:42 +0100

    run new formatter

--- dotfiles/nvim/lua/plugins/ui.lua
+++ dotfiles/nvim/lua/plugins/ui.lua
@@ -1,162 +1,161 @@
 return {
 
-    -- Theme inspired by Atom
-    {
-        'navarasu/onedark.nvim',
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme 'onedark'
-        end,
-    },
+	-- Theme inspired by Atom
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("onedark")
+		end,
+	},
 
-    -- better vim.ui
-    {
-        "stevearc/dressing.nvim",
-        lazy = true,
-        init = function()
-            ---@diagnostic disable-next-line: duplicate-set-field
-            vim.ui.select = function(...)
-                require("lazy").load({ plugins = { "dressing.nvim" } })
-                return vim.ui.select(...)
-            end
-            ---@diagnostic disable-next-line: duplicate-set-field
-            vim.ui.input = function(...)
-                require("lazy").load({ plugins = { "dressing.nvim" } })
-                return vim.ui.input(...)
-            end
-        end,
-    },
+	-- better vim.ui
+	{
+		"stevearc/dressing.nvim",
+		lazy = true,
+		init = function()
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.select = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.select(...)
+			end
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.input = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.input(...)
+			end
+		end,
+	},
 
-    -- indent guides for Neovim
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        event = { "BufReadPost", "BufNewFile" },
-        opts = {
-            -- char = "▏",
-            char = "│",
-            filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-            show_trailing_blankline_indent = false,
-            show_current_context = false,
-        },
-    },
+	-- indent guides for Neovim
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			-- char = "▏",
+			char = "│",
+			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+			show_trailing_blankline_indent = false,
+			show_current_context = false,
+		},
+	},
 
-    -- active indent guide and indent text objects
-    {
-        "echasnovski/mini.indentscope",
-        version = false, -- wait till new 0.7.0 release to put it back on semver
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            -- symbol = "▏",
-            symbol = "│",
-            options = { try_as_border = true },
-        },
-        init = function()
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-                callback = function()
-                    vim.b.miniindentscope_disable = true
-                end,
-            })
-        end,
-        config = function(_, opts)
-            require("mini.indentscope").setup(opts)
-        end,
-    },
+	-- active indent guide and indent text objects
+	{
+		"echasnovski/mini.indentscope",
+		version = false, -- wait till new 0.7.0 release to put it back on semver
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			-- symbol = "▏",
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+		config = function(_, opts)
+			require("mini.indentscope").setup(opts)
+		end,
+	},
 
+	-- Set lualine as statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "onedark",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	},
 
-    -- Set lualine as statusline
-    {
-        'nvim-lualine/lualine.nvim',
-        opts = {
-            options = {
-                icons_enabled = true,
-                theme = 'onedark',
-                component_separators = '|',
-                section_separators = '',
-            },
-        },
-    },
+	-- dashboard
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		opts = function()
+			local dashboard = require("alpha.themes.dashboard")
+			dashboard.section.buttons.val = {
+				dashboard.button("f", " " .. " [f]ind file", ":Telescope find_files <CR>"),
+				dashboard.button("n", " " .. " [n]ew file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("r", " " .. " [r]ecent files", ":Telescope oldfiles <CR>"),
+				dashboard.button("g", " " .. " [g]rep text", ":Telescope live_grep <CR>"),
+				dashboard.button("c", " " .. " [c]onfig", ":e $MYVIMRC <CR>"),
+				dashboard.button("s", " " .. " [s]ession restore", [[:lua require("persistence").load() <cr>]]),
+				dashboard.button("l", "󰒲 " .. " [l]azy menu", ":Lazy<CR>"),
+				dashboard.button("q", " " .. " [q]uit", ":qa<CR>"),
+			}
+			for _, button in ipairs(dashboard.section.buttons.val) do
+				button.opts.hl = "AlphaButtons"
+				button.opts.hl_shortcut = "AlphaShortcut"
+			end
+			dashboard.section.footer.opts.hl = "Type"
+			dashboard.section.header.opts.hl = "AlphaHeader"
+			dashboard.section.buttons.opts.hl = "AlphaButtons"
+			dashboard.opts.layout[1].val = 8
+			return dashboard
+		end,
+		config = function(_, dashboard)
+			-- close Lazy and re-open when the dashboard is ready
+			if vim.o.filetype == "lazy" then
+				vim.cmd.close()
+				vim.api.nvim_create_autocmd("User", {
+					pattern = "AlphaReady",
+					callback = function()
+						require("lazy").show()
+					end,
+				})
+			end
 
-    -- dashboard
-    {
-        "goolord/alpha-nvim",
-        event = "VimEnter",
-        opts = function()
-            local dashboard = require("alpha.themes.dashboard")
-            dashboard.section.buttons.val = {
-                dashboard.button("f", " " .. " [f]ind file", ":Telescope find_files <CR>"),
-                dashboard.button("n", " " .. " [n]ew file", ":ene <BAR> startinsert <CR>"),
-                dashboard.button("r", " " .. " [r]ecent files", ":Telescope oldfiles <CR>"),
-                dashboard.button("g", " " .. " [g]rep text", ":Telescope live_grep <CR>"),
-                dashboard.button("c", " " .. " [c]onfig", ":e $MYVIMRC <CR>"),
-                dashboard.button("s", " " .. " [s]ession restore", [[:lua require("persistence").load() <cr>]]),
-                dashboard.button("l", "󰒲 " .. " [l]azy menu", ":Lazy<CR>"),
-                dashboard.button("q", " " .. " [q]uit", ":qa<CR>"),
-            }
-            for _, button in ipairs(dashboard.section.buttons.val) do
-                button.opts.hl = "AlphaButtons"
-                button.opts.hl_shortcut = "AlphaShortcut"
-            end
-            dashboard.section.footer.opts.hl = "Type"
-            dashboard.section.header.opts.hl = "AlphaHeader"
-            dashboard.section.buttons.opts.hl = "AlphaButtons"
-            dashboard.opts.layout[1].val = 8
-            return dashboard
-        end,
-        config = function(_, dashboard)
-            -- close Lazy and re-open when the dashboard is ready
-            if vim.o.filetype == "lazy" then
-                vim.cmd.close()
-                vim.api.nvim_create_autocmd("User", {
-                    pattern = "AlphaReady",
-                    callback = function()
-                        require("lazy").show()
-                    end,
-                })
-            end
+			require("alpha").setup(dashboard.opts)
 
-            require("alpha").setup(dashboard.opts)
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LazyVimStarted",
+				callback = function()
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					dashboard.section.footer.val = "Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+					pcall(vim.cmd.AlphaRedraw)
+				end,
+			})
+		end,
+	},
 
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "LazyVimStarted",
-                callback = function()
-                    local stats = require("lazy").stats()
-                    local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-                    dashboard.section.footer.val = "Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-                    pcall(vim.cmd.AlphaRedraw)
-                end,
-            })
-        end,
-    },
+	-- ui components (needed for neotree)
+	{ "MunifTanjim/nui.nvim", lazy = true },
 
-    -- ui components (needed for neotree)
-    { "MunifTanjim/nui.nvim",        lazy = true },
+	-- icons
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
 
-    -- icons
-    { "nvim-tree/nvim-web-devicons", lazy = true },
-
-    -- noicer ui
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            lsp = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                },
-            },
-            presets = {
-                bottom_search = true,
-                command_palette = true,
-                long_message_to_split = true,
-            },
-        },
-        -- stylua: ignore
-        keys = {
-            { "<leader>ml", function() require("noice").cmd("last") end,    desc = "Noice Last Message" },
-            { "<leader>mh", function() require("noice").cmd("history") end, desc = "Noice History" },
-            { "<leader>ma", function() require("noice").cmd("all") end,     desc = "Noice All" },
-        },
-    },
+	-- noicer ui
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+				long_message_to_split = true,
+			},
+		},
+		-- stylua: ignore
+		keys = {
+			{ "<leader>ml", function() require("noice").cmd("last") end,    desc = "Noice Last Message" },
+			{ "<leader>mh", function() require("noice").cmd("history") end, desc = "Noice History" },
+			{ "<leader>ma", function() require("noice").cmd("all") end,     desc = "Noice All" },
+		},
+	},
 }
