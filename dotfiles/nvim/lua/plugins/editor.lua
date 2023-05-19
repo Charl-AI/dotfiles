12f33c69471878b3855a32a1fc181a6fdf27f10d
C Jones <cj1917@ic.ac.uk>
2023-05-19 16:35:52 +0100

    remove unused git stage actions

--- dotfiles/nvim/lua/plugins/editor.lua
+++ dotfiles/nvim/lua/plugins/editor.lua
@@ -237,10 +237,7 @@ return {
 
 				map("n", "]h", gs.next_hunk, "Next Hunk")
 				map("n", "[h", gs.prev_hunk, "Prev Hunk")
-				map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
 				map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-				map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
-				map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
 				map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
 				map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
 				map("n", "<leader>gb", function()
