c467d8e3cee29aa8441e10d72f627d94b489474a
C Jones <cj1917@ic.ac.uk>
2023-05-19 15:59:23 +0100

    lower illuminate delay

--- dotfiles/nvim/lua/plugins/editor.lua
+++ dotfiles/nvim/lua/plugins/editor.lua
@@ -260,7 +260,7 @@ return {
 	{
 		"RRethy/vim-illuminate",
 		event = { "BufReadPost", "BufNewFile" },
-		opts = { delay = 200 },
+		opts = { delay = 50 },
 		config = function(_, opts)
 			require("illuminate").configure(opts)
 
