4fc850c7ba967c0e43dd3439827080a49ed4ddbf
C Jones <cj1917@ic.ac.uk>
2023-05-19 15:56:35 +0100

    overhaul telescope bindings and remove spectre

--- dotfiles/nvim/lua/plugins/editor.lua
+++ dotfiles/nvim/lua/plugins/editor.lua
@@ -73,58 +73,38 @@ return {
 		},
 	},
 
-	-- search/replace in multiple files
-	{
-		"windwp/nvim-spectre",
-		-- stylua: ignore
-		keys = {
-			{ "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
-		},
-	},
-
 	-- fuzzy finder
 	{
 		"nvim-telescope/telescope.nvim",
 		cmd = "Telescope",
-		version = false, -- telescope did only one release, so use HEAD for now
+		version = false,
 		keys = {
-			-- commonly used
+			-- commonly used ones have additional mappings with <leader>...
 			{ "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
-			{ "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Find in Files (Grep)" },
-			{ "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+			{ "<leader>?", "<cmd>Telescope live_grep<cr>", desc = "Find in Files (Grep)" },
+			{ "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find in Buffer (Grep)" },
 			{ "<leader><space>", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find Files" },
-			-- find
-			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-			{ "<leader>fF", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-			{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-			-- git
-			{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
-			{ "<leader>gt", "<cmd>Telescope git_status<CR>", desc = "status" },
-			-- search
+			-- +s namespace is for complete list of telescope commands
 			{ "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
-			{ "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+			{ "<leader>sb", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Buffers" },
 			{ "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
 			{ "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
 			{ "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
-			{ "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+			{ "<leader>sg", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Grep (buffer)" },
+			{ "<leader>sG", "<cmd>Telescope live_grep<cr>", desc = "Grep (all files)" },
 			{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
-			{
-				"<leader>sH",
-				"<cmd>Telescope highlights<cr>",
-				desc = "Search Highlight Groups",
-			},
+			{ "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jump list" },
 			{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
 			{ "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
 			{ "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
 			{ "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-			{ "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
 			{ "<leader>sw", "<cmd>Telescope grep_string<cr>", desc = "Word" },
-			{ "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jump list" },
 		},
 		opts = {
 			defaults = {
 				prompt_prefix = " ",
 				selection_caret = " ",
+				initial_mode = "normal",
 				mappings = {
 					i = {
 						["<C-j>"] = function(...)
@@ -156,6 +136,7 @@ return {
 		end,
 		keys = {
 			{ "<leader>p", "<cmd>Telescope neoclip<cr>", desc = "Clipboard History" },
+			{ "<leader>sp", "<cmd>Telescope neoclip<cr>", desc = "Clipboard" },
 		},
 	},
 
@@ -239,7 +220,7 @@ return {
 				["<leader>f"] = { name = "+file/find" },
 				["<leader>g"] = { name = "+git" },
 				["<leader>q"] = { name = "+quit/session" },
-				["<leader>s"] = { name = "+search" },
+				["<leader>s"] = { name = "+search (telescope)" },
 				["<leader>x"] = { name = "+diagnostics/quickfix" },
 				["<leader>m"] = { name = "+messages" },
 			}
@@ -329,7 +310,7 @@ return {
 	{
 		"folke/trouble.nvim",
 		cmd = { "TroubleToggle", "Trouble" },
-		opts = { use_diagnostic_signs = true },
+		opts = { use_diagnostic_signs = true, position = "right" },
 		keys = {
 			{
 				"<leader>xx",
