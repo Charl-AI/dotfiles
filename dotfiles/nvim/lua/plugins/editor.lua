fe8b7be88ccc5a875aa17bf818e04f3905fcbe93
C Jones <cj1917@ic.ac.uk>
2023-05-20 20:55:20 +0100

    switch diagnostics key to x to make more consistent

--- dotfiles/nvim/lua/plugins/editor.lua
+++ dotfiles/nvim/lua/plugins/editor.lua
@@ -79,25 +79,25 @@ return {
 		version = false,
 		keys = {
 			-- commonly used ones have additional mappings with <leader>...
-			{ "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
-			{ "<leader>?", "<cmd>Telescope live_grep<cr>", desc = "Find in Files (Grep)" },
-			{ "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find in Buffer (Grep)" },
-			{ "<leader><space>", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find Files" },
+			{ "<leader>,",       "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
+			{ "<leader>?",       "<cmd>Telescope live_grep<cr>",                     desc = "Find in Files (Grep)" },
+			{ "<leader>/",       "<cmd>Telescope current_buffer_fuzzy_find<cr>",     desc = "Find in Buffer (Grep)" },
+			{ "<leader><space>", "<cmd>Telescope find_files hidden=true<cr>",        desc = "Find Files" },
 			-- +s namespace is for complete list of telescope commands
-			{ "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
-			{ "<leader>sb", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Buffers" },
-			{ "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-			{ "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-			{ "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
-			{ "<leader>sg", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Grep (buffer)" },
-			{ "<leader>sG", "<cmd>Telescope live_grep<cr>", desc = "Grep (all files)" },
-			{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
-			{ "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jump list" },
-			{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-			{ "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-			{ "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-			{ "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-			{ "<leader>sw", "<cmd>Telescope grep_string<cr>", desc = "Word" },
+			{ "<leader>sa",      "<cmd>Telescope autocommands<cr>",                  desc = "Auto Commands" },
+			{ "<leader>sb",      "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Buffers" },
+			{ "<leader>sc",      "<cmd>Telescope command_history<cr>",               desc = "Command History" },
+			{ "<leader>sC",      "<cmd>Telescope commands<cr>",                      desc = "Commands" },
+			{ "<leader>sx",      "<cmd>Telescope diagnostics<cr>",                   desc = "Diagnostics" },
+			{ "<leader>sg",      "<cmd>Telescope current_buffer_fuzzy_find<cr>",     desc = "Grep (buffer)" },
+			{ "<leader>sG",      "<cmd>Telescope live_grep<cr>",                     desc = "Grep (all files)" },
+			{ "<leader>sh",      "<cmd>Telescope help_tags<cr>",                     desc = "Help Pages" },
+			{ "<leader>sj",      "<cmd>Telescope jumplist<cr>",                      desc = "Jump list" },
+			{ "<leader>sk",      "<cmd>Telescope keymaps<cr>",                       desc = "Key Maps" },
+			{ "<leader>sM",      "<cmd>Telescope man_pages<cr>",                     desc = "Man Pages" },
+			{ "<leader>sm",      "<cmd>Telescope marks<cr>",                         desc = "Jump to Mark" },
+			{ "<leader>so",      "<cmd>Telescope vim_options<cr>",                   desc = "Options" },
+			{ "<leader>sw",      "<cmd>Telescope grep_string<cr>",                   desc = "Word" },
 		},
 		opts = {
 			defaults = {
@@ -134,7 +134,7 @@ return {
 			require("telescope").load_extension("neoclip")
 		end,
 		keys = {
-			{ "<leader>p", "<cmd>Telescope neoclip<cr>", desc = "Clipboard History" },
+			{ "<leader>p",  "<cmd>Telescope neoclip<cr>", desc = "Clipboard History" },
 			{ "<leader>sp", "<cmd>Telescope neoclip<cr>", desc = "Clipboard" },
 		},
 	},
@@ -154,7 +154,7 @@ return {
 	{
 		"ggandor/leap.nvim",
 		keys = {
-			{ "s", mode = { "n", "x", "o" }, desc = "Leap (bidirectional)" },
+			{ "s",  mode = { "n", "x", "o" }, desc = "Leap (bidirectional)" },
 			{ "gs", mode = { "n", "x", "o" }, desc = "Leap (other windows)" },
 		},
 		opts = {
@@ -305,12 +305,12 @@ return {
 		opts = { use_diagnostic_signs = true },
 		keys = {
 			{
-				"<leader>d",
+				"<leader>x",
 				"<cmd>TroubleToggle document_diagnostics<cr>",
 				desc = "Document Diagnostics (trouble)",
 			},
 			{
-				"<leader>D",
+				"<leader>X",
 				"<cmd>TroubleToggle workspace_diagnostics<cr>",
 				desc = "Workspace Diagnostics (trouble)",
 			},
