f23ef513386afcf05a4bb781b0f7ae91d57046e9
C Jones <cj1917@ic.ac.uk>
2023-06-05 10:05:42 +0100

    remove dashboard (alpha)

--- dotfiles/nvim/lua/plugins/ui.lua
+++ dotfiles/nvim/lua/plugins/ui.lua
@@ -51,58 +51,6 @@ return {
 		},
 	},
 
-	-- dashboard
-	{
-		"goolord/alpha-nvim",
-		event = "VimEnter",
-		opts = function()
-			local dashboard = require("alpha.themes.dashboard")
-			dashboard.section.buttons.val = {
-				dashboard.button("f", " " .. " [f]ind file", ":Telescope find_files hidden=true<CR>"),
-				dashboard.button("n", " " .. " [n]ew file", ":ene <BAR> startinsert <CR>"),
-				dashboard.button("r", " " .. " [r]ecent files", ":Telescope oldfiles <CR>"),
-				dashboard.button("g", " " .. " [g]rep text", ":Telescope live_grep <CR>"),
-				dashboard.button("c", " " .. " [c]onfig", ":e $MYVIMRC <CR>"),
-				dashboard.button("s", " " .. " [s]ession restore", [[:lua require("persistence").load() <cr>]]),
-				dashboard.button("l", "󰒲 " .. " [l]azy menu", ":Lazy<CR>"),
-				dashboard.button("q", " " .. " [q]uit", ":qa<CR>"),
-			}
-			for _, button in ipairs(dashboard.section.buttons.val) do
-				button.opts.hl = "AlphaButtons"
-				button.opts.hl_shortcut = "AlphaShortcut"
-			end
-			dashboard.section.footer.opts.hl = "Type"
-			dashboard.section.header.opts.hl = "AlphaHeader"
-			dashboard.section.buttons.opts.hl = "AlphaButtons"
-			dashboard.opts.layout[1].val = 8
-			return dashboard
-		end,
-		config = function(_, dashboard)
-			-- close Lazy and re-open when the dashboard is ready
-			if vim.o.filetype == "lazy" then
-				vim.cmd.close()
-				vim.api.nvim_create_autocmd("User", {
-					pattern = "AlphaReady",
-					callback = function()
-						require("lazy").show()
-					end,
-				})
-			end
-
-			require("alpha").setup(dashboard.opts)
-
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "LazyVimStarted",
-				callback = function()
-					local stats = require("lazy").stats()
-					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-					dashboard.section.footer.val = "Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-					pcall(vim.cmd.AlphaRedraw)
-				end,
-			})
-		end,
-	},
-
 	-- ui components (needed for neotree)
 	{ "MunifTanjim/nui.nvim", lazy = true },
 
