298e407f93e2f37c0d4d41b90c6ca63adddc47e7
C Jones <cj1917@ic.ac.uk>
2023-06-26 17:40:50 +0100

    lazy load conda

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -33,18 +33,19 @@ set FZF_ALT_C_OPTS "--preview 'tree -C -a -I '.git' {}'"
 # Use the first conda installation found in the following list
 set -x CONDA_PATH /data/miniconda3/bin/conda /vol/biomedic3/cj1917/miniconda3/bin/conda $HOME/miniconda3/bin/conda
 
-function set_conda_path
+function conda
+    echo "Lazy loading conda upon first invocation..."
+    functions --erase conda
     for conda_path in $CONDA_PATH
         if test -f $conda_path
             echo "Using Conda installation found in $conda_path"
-            eval $conda_path "shell.fish" "hook" $argv | source
+            eval $conda_path "shell.fish" "hook" | source
+            conda $argv
             return
         end
     end
-
+    echo "No conda installation found in $CONDA_PATH"
 end
-
-set_conda_path
 
 # add personal bin to PATH if it exists
 if test -d $HOME/bin
