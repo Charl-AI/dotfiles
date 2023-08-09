2f44b824e1ae658eefe975ae52fb1bd4a2ae18e0
C Jones <cj1917@ic.ac.uk>
2023-08-09 10:41:27 +0100

    move all plugins to init.lua

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -21,8 +21,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ import = "plugins" },
-
 	{
 		"VonHeikemen/lsp-zero.nvim",
 		branch = "v2.x",
@@ -53,6 +51,190 @@ require("lazy").setup({
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 		build = ":TSUpdate",
+	},
+	{
+		"echasnovski/mini.nvim",
+	},
+	{ "tpope/vim-sleuth" },
+	{
+		"christoomey/vim-tmux-navigator",
+		event = { "BufReadPost", "BufNewFile" },
+	},
+	{ "nvim-lua/plenary.nvim", lazy = true },
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			plugins = { spelling = true },
+		},
+		config = function(_, opts)
+			local wk = require("which-key")
+			wk.setup(opts)
+			local keymaps = {
+				mode = { "n", "v" },
+				["g"] = { name = "+goto" },
+				["]"] = { name = "+next" },
+				["["] = { name = "+prev" },
+				["<leader>c"] = { name = "+code" },
+				["<leader>g"] = { name = "+git" },
+				["<leader>s"] = { name = "+search (telescope)" },
+				["<leader><Tab>"] = { name = "+tabs" },
+			}
+			wk.register(keymaps)
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			on_attach = function(buffer)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+				end
+
+				map("n", "]h", gs.next_hunk, "Next Hunk")
+				map("n", "[h", gs.prev_hunk, "Prev Hunk")
+				map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+				map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
+				map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
+				map("n", "<leader>gb", function()
+					gs.blame_line({ full = true })
+				end, "Blame Line")
+				map("n", "<leader>gd", gs.diffthis, "Diff This")
+			end,
+		},
+	},
+	{
+		"folke/trouble.nvim",
+		cmd = { "TroubleToggle", "Trouble" },
+		opts = { use_diagnostic_signs = true },
+		keys = {
+			{
+				"<leader>x",
+				"<cmd>TroubleToggle document_diagnostics<cr>",
+				desc = "Document Diagnostics (trouble)",
+			},
+			{
+				"<leader>X",
+				"<cmd>TroubleToggle workspace_diagnostics<cr>",
+				desc = "Workspace Diagnostics (trouble)",
+			},
+		},
+	},
+	{
+		"folke/todo-comments.nvim",
+		cmd = { "TodoTrouble", "TodoTelescope" },
+		event = { "BufReadPost", "BufNewFile" },
+		config = true,
+		keys = {
+			{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+			{ "<leader>t", "<cmd>TodoTrouble<cr>", desc = "TODO Comments (trouble)" },
+		},
+	},
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("onedark")
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			char = "│",
+			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+			show_trailing_blankline_indent = false,
+			show_current_context = false,
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "onedark",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	},
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+		keys = {
+			{
+				"<leader>r",
+				function()
+					require("persistence").load()
+				end,
+				desc = "Restore Session",
+			},
+		},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		cmd = "Telescope",
+		version = false,
+		keys = {
+			-- commonly used ones get mappings in the top-level namespace: <leader>...
+			{ "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
+			{ "<leader>.", "<cmd>Telescope live_grep<cr>", desc = "Find in Files (Grep)" },
+			{ "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find in Buffer (Fuzzy)" },
+			{ "<leader><space>", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find Files" },
+			{ "<leader>;", "<cmd>Telescope resume<cr>", desc = "Resume Last Search" },
+			-- +s namespace is for less used commands
+			{ "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+			{ "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = "Quickfix List" },
+			{ "<leader>sQ", "<cmd>Telescope quickfixhistory<cr>", desc = "Quickfix History" },
+			{ "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+			{ "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+			{ "<leader>sx", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+			{ "<leader>sl", "<cmd>Telescope loclist<cr>", desc = "Location List" },
+			{ "<leader>sh", "<cmd>Telescope pickers<cr>", desc = "Search History" },
+			{ "<leader>sH", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+			{ "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jump list" },
+			{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+			{ "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+			{ "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+			{ "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+			{ "<leader>sw", "<cmd>Telescope grep_string<cr>", desc = "Word" },
+			-- these go under the +g namespace because they are closer in spirit to the git commands
+			{ "<leader>gf", "<cmd>Telescope git_bcommits<cr>", desc = "Commit History (file)" },
+			{ "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Commit History (project)" },
+			{ "<leader>gs", "<cmd>Telescope git_stash<cr>", desc = "Stash" },
+		},
+		opts = {
+			defaults = {
+				-- cache the last 50 searches
+				cache_picker = { num_pickers = 50 },
+				file_ignore_patterns = { ".git" },
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+				},
+				prompt_prefix = "> ",
+				selection_caret = "-> ",
+				initial_mode = "insert",
+				mappings = {
+					n = {
+						["q"] = function(...)
+							return require("telescope.actions").close(...)
+						end,
+					},
+				},
+			},
+		},
 	},
 }, {})
 
@@ -105,6 +287,51 @@ require("nvim-treesitter.configs").setup({
 			},
 		},
 	},
+})
+
+-- Improved editing experience
+
+require("mini.surround").setup()
+require("mini.pairs").setup()
+require("mini.comment").setup()
+require("mini.move").setup()
+
+local spec_treesitter = require("mini.ai").gen_spec.treesitter
+require("mini.ai").setup({
+	search_method = "cover_or_nearest",
+	-- use treesitter for advanced text objects
+	custom_textobjects = {
+		c = spec_treesitter({ a = "@class.outer", i = "@class.inner" }),
+		m = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
+		f = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
+		a = spec_treesitter({ a = "@parameter.outer", i = "@parameter.inner" }),
+		o = spec_treesitter({
+			a = { "@conditional.outer", "@loop.outer" },
+			i = { "@conditional.inner", "@loop.inner" },
+		}),
+	},
+})
+
+require("mini.indentscope").setup({
+	symbol = "│",
+	draw = { delay = 10 },
+	options = { try_as_border = true },
+})
+
+require("mini.files").setup({
+	mappings = {
+		go_in_plus = "<cr>",
+	},
+})
+vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<cr>", { desc = "Open File View" })
+
+require("mini.jump").setup({
+	highlight = 1,
+})
+-- change default highlighting (underline) to something more visible
+vim.api.nvim_set_hl(0, "MiniJump", {
+	fg = vim.g.terminal_color_0,
+	bg = vim.g.terminal_color_10,
 })
 
 -- LSP stuff
