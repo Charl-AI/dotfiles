6f391dd3be48d3c5e15edde8a8943bcee9868a80
C Jones <cj1917@ic.ac.uk>
2023-08-20 15:37:09 +0100

    make surround more like tpope

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -580,10 +580,33 @@ require("nvim-treesitter.configs").setup({
 
 -- Improved editing experience
 
-require("mini.surround").setup()
 require("mini.pairs").setup()
 require("mini.comment").setup()
 require("mini.move").setup()
+require("mini.surround").setup({
+	custom_surroundings = {
+		["("] = { output = { left = "( ", right = " )" } },
+		["["] = { output = { left = "[ ", right = " ]" } },
+		["{"] = { output = { left = "{ ", right = " }" } },
+		["<"] = { output = { left = "< ", right = " >" } },
+	},
+	mappings = {
+		add = "ys",
+		delete = "ds",
+		find = "",
+		find_left = "",
+		highlight = "",
+		replace = "cs",
+		update_n_lines = "",
+	},
+	search_method = "cover_or_nearest",
+})
+-- Remap adding surrounding to Visual mode selection
+vim.keymap.del("x", "ys")
+vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+-- Make special mapping for "add surrounding for line"
+vim.keymap.set("n", "yss", "ys_", { remap = true })
 
 local spec_treesitter = require("mini.ai").gen_spec.treesitter
 require("mini.ai").setup({
