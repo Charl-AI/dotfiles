03fe6646305c11776b01eeaaa0e00d9fccd7b6ee
C Jones <cj1917@ic.ac.uk>
2024-04-28 18:52:00 +0100

    remove unnecessary line (it is in the defaults anyway)

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -48,9 +48,6 @@ require("lazy").setup({
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
-		opts = {
-			plugins = { spelling = true },
-		},
 		config = function(_, opts)
 			local wk = require("which-key")
 			wk.setup(opts)
