b4002f7ca09c1d81db993ce3c78c4b4820596664
C Jones <cj1917@ic.ac.uk>
2024-05-09 20:59:50 +0100

    add mini bracketed

--- dotfiles/nvim/lua/editor.lua
+++ dotfiles/nvim/lua/editor.lua
@@ -44,6 +44,7 @@ require("nvim-treesitter.configs").setup({
 })
 
 -- mini.nvim is a set of small utilities for improving the editor
+require("mini.bracketed").setup()
 require("mini.comment").setup()
 require("mini.pairs").setup()
 require("mini.move").setup()
