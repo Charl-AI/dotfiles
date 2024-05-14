31fc1b6a0afa4ee3a16bed4cfea7844e04a5f1ad
C Jones <cj1917@ic.ac.uk>
2024-05-14 17:30:31 +0100

    remove unnecessary lines

--- dotfiles/nvim/lua/options.lua
+++ dotfiles/nvim/lua/options.lua
@@ -57,15 +57,10 @@ map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
 -- Move to window using the <ctrl> hjkl keys
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
-
-map("t", "<C-h>", "<cmd>wincmd h<CR>")
-map("t", "<C-j>", "<cmd>wincmd j<CR>")
-map("t", "<C-k>", "<cmd>wincmd k<CR>")
-map("t", "<C-l>", "<cmd>wincmd l<CR>")
+map({ "n", "t" }, "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+map({ "n", "t" }, "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+map({ "n", "t" }, "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+map({ "n", "t" }, "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
 
 -- use esc to go to normal mode in terminal
 map("t", "<esc>", [[<C-\><C-n>]])
@@ -80,16 +75,10 @@ map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window w
 map("i", "<C-H>", "<C-W>", { desc = "Delete last word" })
 
 -- move cursor with alt-hjkl in insert mode
-map("c", "<M-h>", "<Left>", { silent = false, desc = "Left" })
-map("c", "<M-l>", "<Right>", { silent = false, desc = "Right" })
-map("i", "<M-h>", "<Left>", { noremap = false, desc = "Left" })
-map("i", "<M-j>", "<Down>", { noremap = false, desc = "Down" })
-map("i", "<M-k>", "<Up>", { noremap = false, desc = "Up" })
-map("i", "<M-l>", "<Right>", { noremap = false, desc = "Right" })
-map("t", "<M-h>", "<Left>", { desc = "Left" })
-map("t", "<M-j>", "<Down>", { desc = "Down" })
-map("t", "<M-k>", "<Up>", { desc = "Up" })
-map("t", "<M-l>", "<Right>", { desc = "Right" })
+map({ "i", "c", "t" }, "<M-h>", "<Left>", { silent = false, noremap = false, desc = "Left" })
+map({ "i", "c", "t" }, "<M-j>", "<Down>", { silent = false, noremap = false, desc = "Down" })
+map({ "i", "c", "t" }, "<M-k>", "<Up>", { silent = false, noremap = false, desc = "Up" })
+map({ "i", "c", "t" }, "<M-l>", "<Right>", { silent = false, noremap = false, desc = "Right" })
 
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
@@ -104,12 +93,8 @@ map("v", "<leader>#", '"hy:%s/<C-r>h//gc<left><left><left>', { desc = "Find and 
 map("n", "<leader>#", ":%s/<C-r><C-w>//gc<Left><Left><left>", { desc = "Find and replace current word" })
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-map("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map({ "n", "x", "o" }, "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map({ "n", "x", "o" }, "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 
 -- Add undo break-points
 map("i", ",", ",<c-g>u")
@@ -142,20 +127,14 @@ map("n", "<leader>w", "<cmd>set wrap!<cr>", { desc = "[w]ord wrap (toggle)" })
 -- Now we can copy-delete-paste without losing the first copy
 -- To paste from the delete registers use "dp, "cp, "xp
 -- The default delete registers of " and - still work as expected
-map("n", "d", '"dd')
-map("v", "d", '"dd')
-map("n", "D", '"dD')
-map("v", "D", '"dD')
+map({ "n", "v" }, "d", '"dd')
+map({ "n", "v" }, "D", '"dD')
 
-map("n", "x", '"xx')
-map("v", "x", '"xx')
-map("n", "X", '"xX')
-map("v", "X", '"xX')
+map({ "n", "v" }, "x", '"xx')
+map({ "n", "v" }, "X", '"xX')
 
-map("n", "c", '"cc')
-map("v", "c", '"cc')
-map("n", "C", '"cC')
-map("v", "C", '"cC')
+map({ "n", "v" }, "c", '"cc')
+map({ "n", "v" }, "C", '"cC')
 
 -- highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
