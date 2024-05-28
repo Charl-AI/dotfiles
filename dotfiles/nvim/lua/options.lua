683b8cf1eb61c42a06a5d3c9f35133dc9aadcc4d
C Jones <cj1917@ic.ac.uk>
2024-05-28 11:01:39 +0100

    remove unused indent options

--- dotfiles/nvim/lua/options.lua
+++ dotfiles/nvim/lua/options.lua
@@ -104,10 +104,6 @@ map("i", ";", ";<c-g>u")
 -- save file
 map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
--- better indenting
-map("v", "<", "<gv")
-map("v", ">", ">gv")
-
 -- new file
 map("n", "<leader>n", "<cmd>enew<cr>", { desc = "[n]ew File" })
 
