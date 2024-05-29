fe8041818ce86c82290cab49baea521bf2e55327
C Jones <cj1917@ic.ac.uk>
2024-05-29 20:29:32 +0100

    reformat

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,21 +17,21 @@ vim.opt.rtp:prepend(lazypath)
 vim.keymap.set("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "[l]azy (plugin manager)", noremap = true, silent = true })
 require("lazy").setup({
   -- misc
-  { "catppuccin/nvim",                            name = "catppuccin",   priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "nvim-tree/nvim-web-devicons" },
   { "christoomey/vim-tmux-navigator" },
 
   -- editor
   { "folke/which-key.nvim" },
   { "folke/trouble.nvim" },
-  { "nvim-treesitter/nvim-treesitter",            build = ":TSUpdate" },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "nvim-treesitter/nvim-treesitter-textobjects" },
   { "echasnovski/mini.nvim" },
   { "lewis6991/gitsigns.nvim" },
   { "gpanders/nvim-parinfer" },
 
   -- lsp
-  { "williamboman/mason.nvim",                    build = ":MasonUpdate" },
+  { "williamboman/mason.nvim", build = ":MasonUpdate" },
   { "williamboman/mason-lspconfig.nvim" },
   { "stevearc/conform.nvim" },
   { "hrsh7th/vim-vsnip" },
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -38,9 +38,9 @@ opt.fillchars = { eob = " " }
 
 -- show tab characters
 opt.list = true
-opt.listchars:append {
+opt.listchars:append({
   tab = "<->",
-}
+})
 
 if vim.fn.has("nvim-0.9.0") == 1 then
   opt.splitkeep = "screen"
