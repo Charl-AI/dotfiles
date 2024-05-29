8635b5bd6029860690953cab08bf19e623767bbd
C Jones <cj1917@ic.ac.uk>
2024-05-29 18:33:11 +0100

    disable ligatures

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Frappe'
 config.font = wezterm.font 'JetBrains Mono'
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- turn off ligatures
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 10.0
 
