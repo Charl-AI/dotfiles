bf5cbdd58c952467399223fb6370da1b51380e5d
C Jones <cj1917@ic.ac.uk>
2024-05-29 20:28:52 +0100

    reformat

--- .profile
+++ .profile
@@ -1,41 +1,40 @@
 # ~/.profile: executed by the command interpreter for login shells.
 # This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
-# exists. Use this file to set environment variables and add items to PATH. 
-
+# exists. Use this file to set environment variables and add items to PATH.
 
 # add homebrew-installed packages to PATH
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 if [ -x "$(command -v pyenv)" ]; then
-	export PYENV_ROOT="$HOME/.pyenv"
-	export PATH="$PYENV_ROOT/bin:$PATH"
-	eval "$(pyenv init -)"
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
 else
-	echo "pyenv not found. Using system python."
+  echo "pyenv not found. Using system python."
 fi
 
 if [ -x "$(command -v rg)" ]; then
-	export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
+  export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 fi
 
 if [ -x "$(command -v fzf)" ]; then
-	export FZF_DEFAULT_OPTS='--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
+  export FZF_DEFAULT_OPTS='--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
 fi
 
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/bin" ]; then
-	export PATH="$HOME/bin:$PATH"
+  export PATH="$HOME/bin:$PATH"
 fi
 
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/.local/bin" ]; then
-	export PATH="$HOME/.local/bin:$PATH"
+  export PATH="$HOME/.local/bin:$PATH"
 fi
 
 # if running bash
 if [ -n "$BASH_VERSION" ]; then
-	# include .bashrc if it exists
-	if [ -f "$HOME/.bashrc" ]; then
-		. "$HOME/.bashrc"
-	fi
+  # include .bashrc if it exists
+  if [ -f "$HOME/.bashrc" ]; then
+    . "$HOME/.bashrc"
+  fi
 fi
--- install.sh
+++ install.sh
@@ -4,49 +4,49 @@
 set -e
 
 function install_packages {
-	echo "Installing packages"
+  echo "Installing packages"
 
-	brew update &&\
-	brew bundle install --cleanup --file=$HOME/dotfiles/.Brewfile --no-lock &&\
-	brew upgrade
+  brew update &&
+    brew bundle install --cleanup --file=$HOME/dotfiles/.Brewfile --no-lock &&
+    brew upgrade
 
 }
 
 function link_dotfiles {
-	echo "Symlinking dotfiles"
-	stow .
+  echo "Symlinking dotfiles"
+  stow .
 
 }
 
 function main {
-	# if no arguments are passed, install everything and link dotfiles
-	if [ $# -eq 0 ]; then
-		install_packages
-		link_dotfiles
-		return
-	fi
+  # if no arguments are passed, install everything and link dotfiles
+  if [ $# -eq 0 ]; then
+    install_packages
+    link_dotfiles
+    return
+  fi
 
-	# if "-h" or "--help" is passed, print help
-	if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-		echo "Usage: $0 [OPTION]"
-		echo "Install dotfiles and packages"
-		echo ""
-		echo "Options:"
-		echo "  -h, --help     Print this help message"
-		echo "  -p, --packages Install packages"
-		echo "  -d, --dotfiles Link dotfiles"
-		return
-	fi
+  # if "-h" or "--help" is passed, print help
+  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 [OPTION]"
+    echo "Install dotfiles and packages"
+    echo ""
+    echo "Options:"
+    echo "  -h, --help     Print this help message"
+    echo "  -p, --packages Install packages"
+    echo "  -d, --dotfiles Link dotfiles"
+    return
+  fi
 
-	# if "-p" or "--packages" is passed, install packages
-	if [ "$1" = "-p" ] || [ "$1" = "--packages" ]; then
-		install_packages
-	fi
+  # if "-p" or "--packages" is passed, install packages
+  if [ "$1" = "-p" ] || [ "$1" = "--packages" ]; then
+    install_packages
+  fi
 
-	# if "-d" or "--dotfiles" is passed, link dotfiles
-	if [ "$1" = "-d" ] || [ "$1" = "--dotfiles" ]; then
-		link_dotfiles
-	fi
+  # if "-d" or "--dotfiles" is passed, link dotfiles
+  if [ "$1" = "-d" ] || [ "$1" = "--dotfiles" ]; then
+    link_dotfiles
+  fi
 
 }
 
